78c77d38ebf2c9d3fbd3d6336608f29e46710de4
Uriel Lara <141281252+UrieLara@users.noreply.github.com>
2024-08-05 11:10:59 -0300

    movement characters fixed
    - se agregó movimiento y rotación adecuada a los personajes.
    - se agregó sonido de pasos al player.
    - se delimitó área para no salir del escenario.

--- Assets/Scripts/MoveEntity.cs
+++ Assets/Scripts/MoveEntity.cs
@@ -6,30 +6,29 @@ public abstract class MoveEntity : MonoBehaviour
 {
     public float turnSmoothTime = 0.2f;
     float turnSmoothVelocity;
-  
-    public Vector3 RotateCharacter(Vector3 direction)
-    {
 
-        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+     Vector3 RotateCharacterBase(Vector3 direction, float targetAngleOffset = 0f)
+    {
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + targetAngleOffset;
         float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
 
         Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0) * Vector3.forward;
 
         transform.rotation = Quaternion.Euler(0f, angle, 0f);
-        
+
         return moveDir;
+    }
+
+    public Vector3 RotateCharacter(Vector3 direction)
+    {
+        return RotateCharacterBase(direction);
     }
 
     public Vector3 RotateCharacter(Vector3 direction, Transform target)
     {
-
-        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + target.eulerAngles.y;
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-
-        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0) * Vector3.forward;
-
-        transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-        return moveDir;
+        float targetAngleOffset = target.eulerAngles.y;
+        return RotateCharacterBase(direction, targetAngleOffset);
     }
+
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.ConstrainedExecution;
@@ -7,15 +8,23 @@ using UnityEngine.PlayerLoop;
 public class Player : MoveEntity
 {
     CharacterController controller;
-
     public float speed = 4;
+    Vector3 lastPosition;
+    float footstepDistance = 1.2f;
+
+
     public Transform cam;
+
+    AudioSource audioSource;
+  //  public AudioClip footstepSound;
 
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        audioSource = GetComponent<AudioSource>();
 
+        lastPosition = transform.position;
     }
 
     private void Update()
@@ -24,21 +33,33 @@ public class Player : MoveEntity
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
 
+        float distance = Vector3.Distance(transform.position, lastPosition);
+
+        if (distance >= footstepDistance)
+        {
+            PlaySoundWalk();
+        }
+
         if (hor != 0 || ver != 0)
         {
-            Vector3 movement = new Vector3(hor, 0, ver);
-            MovePlayer(movement);
-
-            transform.position = new Vector3(this.transform.position.x, -1, this.transform.position.z);
+           Vector3 movement = new Vector3(hor, -1, ver);
+           MovePlayer(movement);
         }
+
     }
 
     void MovePlayer(Vector3 movement)
     {
-
         Vector3 moveDir;
         moveDir = RotateCharacter(movement, cam);
         controller.Move(moveDir.normalized * speed * Time.deltaTime);
+    }
+
+    void PlaySoundWalk()
+    {
+        audioSource.Play();
+        lastPosition = transform.position;
+
     }
 
 }
